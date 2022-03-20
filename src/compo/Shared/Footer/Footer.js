ad246c4ffc30b19299f7a416fb4646cb3736db0e
khan-shimul <khanshimul196@gmail.com>
2022-03-20 21:07:56 +0600

    added footer

--- src/compo/Shared/Footer/Footer.js
+++ src/compo/Shared/Footer/Footer.js
@@ -1,9 +1,17 @@
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 const Footer = () => {
     return (
         <footer>
-            <h2>This is Footer</h2>
+            <Box variant="div">
+                <Typography
+                    variant="body2"
+                    sx={{ background: '#404D5F', textAlign: 'center', color: '#fff', p: 3 }}
+                >
+                    All Rights Reserve © Shimul Khan 2022. Made with love for great people.
+                </Typography>
+            </Box>
         </footer>
     );
 };
